f6c9bf23b348a05a6630403115fffba024012191
VaradharajanVR <73377548+VaradharajanVR@users.noreply.github.com>
2020-11-10 15:38:40 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "Diafoni04";
-$db = "onlineshop";
+$servername = "ec2-3-211-176-230.compute-1.amazonaws.com";
+$username = "kkdttekwzgzpzf";
+$password = "353d4a03365031e5268598c2900d14bd8fc9ee2c987100be7607cb82dcfee52c";
+$db = "dfb0au6ml6gppq";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
